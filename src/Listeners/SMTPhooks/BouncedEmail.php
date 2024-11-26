28f45fa7685120dc4399308e86f2a82bb48487f4
Mahmoud Abas <mmabas77@gmail.com>
2024-11-27 01:54:42 +0200

    Handle smtp email bounced permanently using EmailWebhookService

--- src/Listeners/SMTPhooks/BouncedEmail.php
+++ src/Listeners/SMTPhooks/BouncedEmail.php
@@ -3,6 +3,8 @@
 namespace Sendportal\Base\Listeners\SMTPhooks;
 
 use jdavidbakr\MailTracker\Events\PermanentBouncedMessageEvent;
+use Sendportal\Base\Models\Message;
+use Sendportal\Base\Services\Webhooks\EmailWebhookService;
 
 class BouncedEmail
 {
@@ -26,6 +28,10 @@ class BouncedEmail
      */
     public function handle(PermanentBouncedMessageEvent $event)
     {
+        Log::log('info', 'Email bounced permanently', ['event' => $event]);
+        $msg = Message::query()->where('message_id', $event->sent_email->getHeader('X-Mailer-Hash'))->first();
+        $emailWebhookService = new EmailWebhookService();
+        $emailWebhookService->handlePermanentBounce($msg->message_id, now());
         // Access the model using $event->sent_email
         // Access the IP address that triggered the event using $event->ip_address
     }
