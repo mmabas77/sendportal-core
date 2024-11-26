8d4de26ed44491e8147879bd303f6fe16badf023
Mahmoud Abas <mmabas77@gmail.com>
2024-11-27 01:46:15 +0200

    Handle smtp email viewed using EmailWebhookService

--- src/Listeners/SMTPhooks/EmailViewed.php
+++ src/Listeners/SMTPhooks/EmailViewed.php
@@ -4,6 +4,8 @@ namespace Sendportal\Base\Listeners\SMTPhooks;
 
 use Illuminate\Support\Facades\Log;
 use jdavidbakr\MailTracker\Events\ViewEmailEvent;
+use Sendportal\Base\Models\Message;
+use Sendportal\Base\Services\Webhooks\EmailWebhookService;
 
 class EmailViewed
 {
@@ -22,12 +24,16 @@ class EmailViewed
     /**
      * Handle the event.
      *
-     * @param  ViewEmailEvent  $event
+     * @param ViewEmailEvent $event
      * @return void
      */
     public function handle(ViewEmailEvent $event)
     {
         Log::log('info', 'Email viewed', ['event' => $event]);
+        $msg = Message::query()->where('message_id', $event->sent_email->getHeader('X-Mailer-Hash'))->first();
+        $ip = $event->ip_address;
+        $emailWebhookService = new EmailWebhookService();
+        $emailWebhookService->handleOpen($msg->message_id, now(), $ip);
         // Access the model using $event->sent_email
         // Access the IP address that triggered the event using $event->ip_address
     }
