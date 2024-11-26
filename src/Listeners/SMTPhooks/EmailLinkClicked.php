5a5fe82ac7e8a6baff5e145610281d239a9b4f9c
Mahmoud Abas <mmabas77@gmail.com>
2024-11-27 01:58:23 +0200

    Handle smtp email link click using EmailWebhookService

--- src/Listeners/SMTPhooks/EmailLinkClicked.php
+++ src/Listeners/SMTPhooks/EmailLinkClicked.php
@@ -2,7 +2,10 @@
 
 namespace Sendportal\Base\Listeners\SMTPhooks;
 
+use Illuminate\Support\Facades\Log;
 use jdavidbakr\MailTracker\Events\LinkClickedEvent;
+use Sendportal\Base\Models\Message;
+use Sendportal\Base\Services\Webhooks\EmailWebhookService;
 
 class EmailLinkClicked
 {
@@ -21,11 +24,16 @@ class EmailLinkClicked
     /**
      * Handle the event.
      *
-     * @param  LinkClickedEvent  $event
+     * @param LinkClickedEvent $event
      * @return void
      */
     public function handle(LinkClickedEvent $event)
     {
+        Log::log('info', 'Email link clicked', ['event' => $event]);
+        $msg = Message::query()->where('message_id', $event->sent_email->getHeader('X-Mailer-Hash'))->first();
+        $ip = $event->ip_address;
+        $emailWebhookService = new EmailWebhookService();
+        $emailWebhookService->handleClick($msg->message_id, now(), $event->link_url);
         // Access the model using $event->sent_email
         // Access the IP address that triggered the event using $event->ip_address
     }
