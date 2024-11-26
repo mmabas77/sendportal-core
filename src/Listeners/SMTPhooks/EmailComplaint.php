ec919982a3c0ab0abeaf76242a35f089e69b19cc
Mahmoud Abas <mmabas77@gmail.com>
2024-11-27 01:55:45 +0200

    Handle smtp email complaint using EmailWebhookService

--- src/Listeners/SMTPhooks/EmailComplaint.php
+++ src/Listeners/SMTPhooks/EmailComplaint.php
@@ -2,7 +2,10 @@
 
 namespace Sendportal\Base\Listeners\SMTPhooks;
 
+use Illuminate\Support\Facades\Log;
 use jdavidbakr\MailTracker\Events\ComplaintMessageEvent;
+use Sendportal\Base\Models\Message;
+use Sendportal\Base\Services\Webhooks\EmailWebhookService;
 
 class EmailComplaint
 {
@@ -26,6 +29,10 @@ class EmailComplaint
      */
     public function handle(ComplaintMessageEvent $event)
     {
+        Log::log('info', 'Email complaint', ['event' => $event]);
+        $msg = Message::query()->where('message_id', $event->sent_email->getHeader('X-Mailer-Hash'))->first();
+        $emailWebhookService = new EmailWebhookService();
+        $emailWebhookService->handleComplaint($msg->message_id, now());
         // Access the model using $event->sent_email
         // Access the IP address that triggered the event using $event->ip_address
     }
